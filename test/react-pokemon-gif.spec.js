8e569a6cc5feb74fc317cf091e3bf7eccf88a535
Jack Zhang <15jzhang@gmail.com>
2016-08-11 00:20:30 -0400

    Set up React unit tests with mocha, chai & enzyme

---
+++ test/react-pokemon-gif.spec.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+
+import PokemonGif from './../lib/react-pokemon-gif';
+
+describe('react-pokmemon-gif', () => {
+
+});
